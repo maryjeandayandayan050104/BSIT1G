98014fa04a3a1df0d6e5da8a346c25bb773ffb1f
maryjeandayandayan050104 <105650244+maryjeandayandayan050104@users.noreply.github.com>
2023-07-08 11:43:42 +0800

    Add files via upload

---
+++ BSIT1G/midterm-exam.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+/*
+int main()
+{
+    int arr[20], b=0, max=0,min=0;
+    cout<<"\n\n";
+    cout<<"\t\t Highest and Lowest Value Checker"; //44
+    cout<<"\n\n";
+    cout<<"\tHow many items to be process?:";
+    cin>> b ; // 45
+    cout<<"\n\n";
+    cout<<"\tEnter items values:";
+    for(int a=0;a<b;a++)
+        cin>> arr[a]; //46
+    max=arr[0];
+    for (int a=0; a<b; a++)
+    {
+        if(max<arr[a])
+            max=arr[a]; // 47
+    }
+    min=arr[0];
+    for(int a=0;a<b;a++) // 48
+    {
+        if (min>arr[a])
+            min=arr[a];
+    }
+    cout<<"\t ====== DISPLAY RESULT======";
+    cout <<"\tThe Largest Value is " << max <<"."<<endl; // 49
+    cout<<"\tThe Smallest Value is "<< min <<"."<<endl; // 50
+    cout<<"\tEnd of Program"<<endl;
+    return 0;
+}
+*/
+
+
+/*
+int main(){
+    bool found = true;
+    bool flag = false;
+    int num = 1;
+    double x = 5.2;
+    double y = 3.4;
+    int a = 5, b = 8;
+    int n = 20;
+    char ch = 'B';
+
+    cout << "Line 1: !found evaluates to " << !found << endl;
+    cout << "Line 2: x > 4.0 evaluates to " << (x > 4,0) << endl;
+    cout << "Line 3: !num evaluates to " << !num << endl;
+    cout << "Line 4: !found && (x >= 0) evaluates to " << (!found && (x >= 0)) << endl;
+    cout << "Line 5: !(found && (x >= 0) evaluates to " << (!(found && (x >= 0))) << endl;
+    cout << "Line 6: x + y <= 20.5 evaluates to " << (x + y <= 20.5) << endl;
+    cout << "Line 7: (n >= 0) && (n <= 100) evaluates to " << ((n >= 0) && (n <= 100)) << endl;
+    cout << "Line 8: ('A' <= ch && ch <= 'Z') evaluates to " << ('A' <= ch && ch <= 'Z') << endl;
+    cout << "Line 9: (a + 2 <= b) && !flag evaluates to " << ((a + 2 <= b) && !flag) << endl;
+    return 0;
+
+}
+*/
+
