3d264afe342f066bc688feac243b143c59e54d25
maryjeandayandayan050104 <105650244+maryjeandayandayan050104@users.noreply.github.com>
2023-07-08 11:40:58 +0800

    Add files via upload

---
+++ BSIT1G/calendar-activity.cpp
@@ -0,0 +1,20 @@
+/*
+    NAME: DAYANDAYAN, MARY JEAN C.
+    SECTION: BSIT 1G
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << " \t\t\tMARCH 2023\n\n";
+    cout << " Sun\tMon\tTue\tWed\tThur\tFri\tSat\n\n";
+    cout << " \t\t 1\t 2\t 3\t 4\t 5\n";
+    cout << " 6\t 7\t 8\t 9\t 10\t 11\t 12\n";
+    cout << " 13\t 14\t 15\t 16\t 17\t 18\t 19\n";
+    cout << " 20\t 21\t 22\t 23\t 24\t 25\t 26\n";
+    cout << " 27\t 28\t 29\t 30\t\n\n";
+
+    return 0;
+
+}
